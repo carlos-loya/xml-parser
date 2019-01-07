587998488a0f6d8f334740bc6e520e305478c0b9
carlos-loya <exsorest@gmail.com>
2019-01-06 19:52:59 -0600

    Added main.py and requirements template

---
+++ main.py
@@ -0,0 +1,44 @@
+import argparse
+import os
+import io
+import time
+import re
+
+DEBUG = True
+BUFFER_SIZE = 4096
+PATTERN = r"<record>[\s\S]*?<\/record>"
+
+def main(input_file, output_file):
+    direct_path = os.path.dirname(os.path.realpath(__file__))
+    compiled_pattern = re.compile(PATTERN)
+
+    with io.open(file="{path}/{file}".format(path=direct_path,file=input_file), mode="r",buffering=BUFFER_SIZE) as buff_reader, \
+         io.open(file="{path}/{file}".format(path=direct_path,file=output_file), mode="w",buffering=BUFFER_SIZE) as buff_writer:
+
+        buffer = buff_reader.read(BUFFER_SIZE)
+        iterations = 0
+
+        while buffer and iterations<10:
+            is_record = compiled_pattern.search(string=buffer)
+
+            if is_record:
+                record = buffer[:is_record.end()]+"\n"
+                buffer = buffer[is_record.end():]
+                iterations = 0
+
+                buff_writer.write(record)
+            iterations += 1
+            buffer += buff_reader.read(BUFFER_SIZE)
+
+        if buffer:
+            buff_writer.write(buffer)
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Processing raw character streams into XML.')
+    parser.add_argument('--input', metavar='i', type=str, help='Input file name.')
+    parser.add_argument('--output', metavar='o', type=str, help='Output file name.')
+
+    args = parser.parse_args()
+
+    main(input_file=args.input, output_file=args.output)
